c3e566b11e7cbad76c8b0829bc0c38e10cfea952
An Gyubin <bkgr4949p@icloud.com>
2022-08-04 01:54:49 +0900

    add batchnorm in model

--- model.py
+++ model.py
@@ -17,6 +17,12 @@ class TestModel(nn.Module):
         self.fc1 = nn.Linear(7*7,16)
         self.fc2 = nn.Linear(16,10)
 
+        self.bn2d_1 = nn.BatchNorm2d(4)
+        self.bn2d_2 = nn.BatchNorm2d(1)
+
+        self.bn1d_1 = nn.BatchNorm1d(16)
+        self.bn1d_2 = nn.BatchNorm1d(10)
+
         if prob != 0:
             self.dropout1 = nn.Dropout(p=prob)
             self.dropout2 = nn.Dropout(p=prob)
@@ -25,12 +31,12 @@ class TestModel(nn.Module):
     def forward(self, x : Tensor):
 
         x = self.relu(self.dropout1(self.conv1(x)))
-        x = self.pooling(self.relu(self.dropout2(self.conv2(x))))
-        x = self.pooling(self.relu(self.dropout3(self.conv3(x))))
+        x = self.bn2d_1(self.pooling(self.relu(self.dropout2(self.conv2(x)))))
+        x = self.bn2d_2(self.pooling(self.relu(self.dropout3(self.conv3(x)))))
 
         x = self.flatten(x)
 
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        x = self.bn1d_1(self.relu(self.fc1(x)))
+        x = self.bn1d_2(self.relu(self.fc2(x)))
 
         return F.log_softmax(x)
